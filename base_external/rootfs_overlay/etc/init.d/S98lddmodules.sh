65e535c7c778887051d63d1b1303b163f0f7a9af
samu7988 <samu7988@colorado.edu>
2022-03-14 01:01:53 -0600

    Adding load and unload script at startup to bypass the error

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -6,12 +6,16 @@ case "$1" in
 		/usr/bin/module_load faulty
 		/usr/bin/scull_load
 		modprobe hello
+         	echo "starting aesd character driver ..."
+         	start-stop-daemon -S -n aesdchar_load -a /usr/bin/aesdchar_load        
 		;;
 	stop)
 		echo 'Stop scull and misc-modules on shutdown'
 		/usr/bin/module_unload faulty
 		/usr/bin/scull_unload
 		rmmod hello
+		echo "stopping aesd character driver ..."
+         	start-stop-daemon -S -n aesdchar_unload -a /usr/bin/aesdchar_unload
 		;;
 	*)
 	echo "Usage of script: $0 {start|stop} - to load and unload modules"
