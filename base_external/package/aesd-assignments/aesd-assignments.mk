21ad43baee70f4e506f52dd2f1536e47c73faa87
samu7988 <samu7988@colorado.edu>
2022-04-08 23:54:20 -0600

    Added timer functioality , verified working on target

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 5a9d1255f57db55a8055cee767359ff12e62d56e
+AESD_ASSIGNMENTS_VERSION = e837cc9f65d337c2accd873adac0f4e9f209d6e2
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
