674f176061ad0fa14c6b8811ea69c2523e5ff9a5
samu7988 <samu7988@colorado.edu>
2022-04-08 19:55:06 -0600

    Adding working hello world project

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = be18234af5ded9b56d6f3911872bef9651de36ad
+AESD_ASSIGNMENTS_VERSION = 5a9d1255f57db55a8055cee767359ff12e62d56e
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
